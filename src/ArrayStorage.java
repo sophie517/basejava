339ec198de0905fa9569205cbb0969b2e5ad5e12
sophie517 <sophieartemeva@gmail.com>
2024-07-31 10:45:52 +0300

    implement hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,48 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int numOfResumes = 0;
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, numOfResumes, null);
+        numOfResumes = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[numOfResumes++] = resume;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < numOfResumes; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < numOfResumes; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                System.arraycopy(storage, i + 1, storage, i, (numOfResumes - i - 1));
+                numOfResumes--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, numOfResumes);
     }
 
     int size() {
-        return 0;
+        return numOfResumes;
     }
 }
