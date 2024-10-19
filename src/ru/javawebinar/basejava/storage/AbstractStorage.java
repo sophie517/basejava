2590d8d6e62914f6baaf1fe859cb40b9bb6fa607
sophie517 <sophieartemeva@gmail.com>
2024-10-19 18:44:58 +0300

    implement logging

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,9 +6,11 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<T> implements Storage {
     private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     public final void clear() {
         clearResumes();
@@ -17,6 +19,7 @@ public abstract class AbstractStorage<T> implements Storage {
     protected abstract void clearResumes();
 
     public final void update(Resume resume) {
+        LOG.info("Update " + resume);
         T searchKey = getExistingSearchKey(resume.getUuid());
         updateResume(searchKey, resume);
     }
@@ -24,6 +27,7 @@ public abstract class AbstractStorage<T> implements Storage {
     protected abstract void updateResume(T searchKey, Resume resume);
 
     public final void save(Resume resume) {
+        LOG.info("Save " + resume);
         T searchKey = getNotExistingSearchKey(resume.getUuid());
         saveResume(searchKey, resume);
     }
@@ -31,6 +35,7 @@ public abstract class AbstractStorage<T> implements Storage {
     protected abstract void saveResume(T searchKey, Resume resume);
 
     public final Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         T searchKey = getExistingSearchKey(uuid);
         return getResume(searchKey);
     }
@@ -38,6 +43,7 @@ public abstract class AbstractStorage<T> implements Storage {
     protected abstract Resume getResume(T searchKey);
 
     public final void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         T searchKey = getExistingSearchKey(uuid);
         deleteResume(searchKey);
     }
@@ -46,6 +52,7 @@ public abstract class AbstractStorage<T> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> storageList = getAllResumes();
         storageList.sort(RESUME_COMPARATOR);
         return storageList;
@@ -62,6 +69,7 @@ public abstract class AbstractStorage<T> implements Storage {
     protected final T getExistingSearchKey(String uuid) {
         T searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -70,6 +78,7 @@ public abstract class AbstractStorage<T> implements Storage {
     protected final T getNotExistingSearchKey(String uuid) {
         T searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
