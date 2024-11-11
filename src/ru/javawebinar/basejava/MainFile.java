b3e55827a5798860100ec6f95104ae6ad27a345c
sophie517 <sophieartemeva@gmail.com>
2024-11-11 16:18:34 +0300

    implement recursive output

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -8,17 +8,19 @@ public class MainFile {
     public static void main(String[] args) {
         // рекурсивный обход и вывод имени файлов в каталогах и подкаталогах
         File path = new File("./src/ru/javawebinar/basejava");
-        printAllFiles(path.toPath());
+        printAllFiles(path.toPath(), 0);
     }
 
-    public static void printAllFiles(Path path) {
+    public static void printAllFiles(Path path, int indentLevel) {
+        String indent = " ".repeat(indentLevel * 2);
         if (Files.isRegularFile(path)) {
-            System.out.println(path.getFileName());
+            System.out.println(indent + "f: " + path.getFileName());
         } else if (Files.isDirectory(path)){
+            System.out.println(indent + "d: " + path.getFileName());
             File[] files = path.toFile().listFiles();
             if (files != null) {
                 for (File file : files) {
-                    printAllFiles(file.toPath());
+                    printAllFiles(file.toPath(), indentLevel + 1);
                 }
             }
         }
